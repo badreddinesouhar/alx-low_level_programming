185116ec82cf33805b8b25946f9483268b68d6ce
badreddinesouhar <badrsouhar@gmail.com>
2023-02-20 09:18:50 -0800

    first C project

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", msg);
+	return (1);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	char *s = "Size of";
+
+	printf("%s a char: %d byte(s)\n", s, sizeof(char));
+	printf("%s an int: %d byte(s)\n", s, sizeof(int));
+	printf("%s a long int: %d byte(s)\n", s, sizeof(long int));
+	printf("%s a long long int: %d byte(s)\n", s, sizeof(long long int));
+	printf("%s a float: %d byte(s)\n", s, sizeof(float));
+	return (0);
+}
