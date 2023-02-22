79b6cdd2de4b83fd7c10f44480bc9fb97eca2232
badreddinesouhar <badrsouhar@gmail.com>
2023-02-22 10:51:53 -0800

    task complete

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * main - entry point
- * print a string
- * return 0 (success)
- * */
+  * main - entry point
+  * Description:print a string
+  * Return: 0 (success)
+  */
 
 int main(void)
 {
-	int i;
-	char *str = "_putchar";
-	i = 0;
+	int i = 0;
+	char *str = "_putchar\n";
 
 	while (str[i])
 	{
-		putchar(1, str[i], 1);
+		putchar(str[i]);
 		i++;
 	}
 	return (0);
-
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,6 @@
 #define MAIN_H
 #include <stdio.h>
 
-void _putchar(int c)
+void _putchar(int c);
+
+#endif
